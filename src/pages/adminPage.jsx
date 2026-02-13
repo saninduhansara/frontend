aef91df1d18699de56ef299b5471bbfa69d8ca7a
saninduhansara <e22130@eng.pdn.ac.lk>
2026-02-13 19:24:22 +0530

    D12

--- src/pages/adminPage.jsx
+++ src/pages/adminPage.jsx
@@ -1,11 +1,22 @@
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
+import { FaBoxArchive } from "react-icons/fa6";
+import { FaShoppingBag } from "react-icons/fa";
+import { IoPeople } from "react-icons/io5";
+import { IoMdSettings } from "react-icons/io";
+
+
 
 export default function AdminPage() {
     return(
-        <div className="w-full h-screen bg-red-900 flex">
-            <div className="w-[300px] h-full bg-white ">
+        <div className="w-full h-screen flex">
+            <div className="w-[300px] h-full flex flex-col items-center  ">
+                <span className="text-3xl font-bold my-5"> Admin Panel</span>
+                <Link className="flex flex-row h-[60px] w-full items-center p-[20px] border text-xl gap-[5px] " to="/admin/products"> <FaBoxArchive /> Products</Link>
+                <Link className="flex flex-row h-[60px] w-full items-center p-[20px] border text-xl gap-[5px]" to="/admin/orders"> <FaShoppingBag /> orders</Link>
+                <Link className="flex flex-row h-[60px] w-full items-center p-[20px] border text-xl gap-[5px]" to="/admin/users"> <IoPeople /> users</Link>
+                <Link className="flex flex-row h-[60px] w-full items-center p-[20px] border text-xl gap-[5px]" to="/admin/settings"> <IoMdSettings /> settings</Link>
             </div>
-            <div className="w-[calc(100%-300px)]  bg-blue-900 h-full ">
+            <div className="w-[calc(100%-300px)]  h-full ">
                 <Routes path="/">
                     <Route path="/" element={<h1>Dashboard</h1>}/>
                     <Route path="/products" element={<h1>Products</h1>}/>
--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,10 +1,60 @@
-export default function LoginPage() {
-    return(
-        <div>
-            <span>
-                This is Login Page
-            </span>
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios  from "axios";
+import toast from "react-hot-toast";
 
+export default function LoginPage() {
+    const [password,setpassword] = useState("")
+    const [email,setemail] = useState("")
+    const navigate = useNavigate()
+
+    function login(){
+        axios.post(import.meta.env.VITE_BACKEND_URL+"/api/users/login",{
+            email: email,
+            password: password
+        }).then((res)=>{
+            console.log(res.data)
+            localStorage.setItem("token",res.data.token)
+            //alert("Login Successful")
+            toast.success("Login Successful")
+            if (res.data.role == "admin"){
+                navigate("/admin")
+
+            }else if(res.data.role == "user"){
+                navigate("/")
+
+            }
+        }).catch((err)=>{
+            console.log(err)
+            //alert("Login Failed")
+            toast.error("Login Failed")
+        })
+     
+    }
+    return(
+        <div className="w-full h-screen bg-[url(./loginbg.jpg)] bg-cover bg-center flex justify-center items-center">
+            <div className="w-[500px] h-[500px] backdrop-blur-sm rounded-[30px] relative gap-[20px] text-white flex flex-col items-center justify-center">
+                <h1 className="absolute top-[20px] text-2xl font-bold text-center my-5">Login</h1>
+                <div className="w-[350px]  flex flex-col ">
+                    <span className="text-lg ">Email</span>
+                    <input onChange={
+                        (e)=>{
+                            setemail(e.target.value)
+                        }
+                    }type="text" className="w-[350px] h-[40px] border border-white rounded-xl"/>
+                </div>
+                <div className="w-[350px]  flex flex-col ">
+                    <span className="text-lg ">Password</span>
+                    <input onChange={
+                        (e)=>{
+                            setpassword(e.target.value)
+                        }
+                    } type="password" className="w-[350px] h-[40px] border border-white rounded-xl"/>
+                </div>
+                <button onClick={login} className="w-[350px] h-[40px] bg-blue-500 text-white rounded-xl mt-5">Login</button>
+                <p>Don't have an account <Link to="/register" className="text-blue-500">sign up</Link> here</p>
+                
+            </div>
         </div>
     )
 }
